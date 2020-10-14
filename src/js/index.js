f91c19ca8cc0746277c66c06fde7c59c5332b739
Luis DeLeon <luismdeleon@gmail.com>
2020-10-14 11:40:16 -0700

    Revert "trying simple web component"
    This reverts commit 0e3dd460f35e546cb10507b66891e55c75704326.

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,5 @@
-import AppFooter from './components/AppFooter.js';
-
 const navToggle = document.querySelector('.nav-toggle');
-const navLinks = document.querySelectorAll('.nav__link');
+const navLinks = document.querySelectorAll('.nav__link')
 
 navToggle.addEventListener('click', () => {
   document.body.classList.toggle('nav-open');
@@ -12,5 +10,3 @@ navLinks.forEach((link) => {
     document.body.classList.remove('nav-open');
   });
 });
-
-window.customElements.define('app-footer', AppFooter);
