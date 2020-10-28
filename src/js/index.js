0ddaba3914124eadad7ee2a955912b9d58b54fb3
Luis DeLeon <luismdeleon@gmail.com>
2020-10-28 13:47:09 -0700

    add contact form

--- src/js/index.js
+++ src/js/index.js
@@ -1,16 +1,36 @@
+/**
+ * Navigation
+ */
 const navToggle = document.querySelector('.nav-toggle');
-const navLinks = document.querySelectorAll('.nav__link');
-
 navToggle.addEventListener('click', () => {
   document.body.classList.toggle('nav-open');
 });
 
+const navLinks = document.querySelectorAll('.nav__link');
 navLinks.forEach((link) => {
   link.addEventListener('click', () => {
     document.body.classList.remove('nav-open');
   });
 });
 
+/**
+ * Contact Form
+ */
+const contactForm = document.querySelector('#contact');
+contactForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  // TODO: Use firestore to store (offline) then submit when back online
+
+  // Reset form fields
+  contactForm.querySelector('#name').value = '';
+  contactForm.querySelector('#email').value = '';
+  contactForm.querySelector('#comment').value = '';
+});
+
+/**
+ * Service worker
+ */
 // Check that service workers are supported
 if ('serviceWorker' in navigator) {
   // Use the window load event to keep the page load performant
