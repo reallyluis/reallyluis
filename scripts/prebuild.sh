d225e69eb6b84805e03d1495fb125637d04e1809
Luis DeLeon <luismdeleon@gmail.com>
2020-12-09 11:31:23 -0800

    add back robots and sitemap files

--- scripts/prebuild.sh
+++ scripts/prebuild.sh
@@ -8,6 +8,8 @@ mkdir public
 mkdir public/css
 mkdir public/js
 
-# Copy image and manifest files to build directory
+# Copy image, manifest and other files to build directory
 cp -R ./src/img ./public/img
+cp ./src/robots.txt ./public/robots.txt
+cp ./src/sitemap-file.xml ./public/sitemap-file.xml
 cp ./src/manifest.webmanifest ./public/manifest.webmanifest
