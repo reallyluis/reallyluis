961a01e53496cf153bb0ebd1b7955bcded9f8e28
Luis DeLeon <luismdeleon@gmail.com>
2020-12-16 13:26:58 -0800

    add TODO to automate version number on build

--- scripts/minify-js.js
+++ scripts/minify-js.js
@@ -10,6 +10,10 @@ const options = {
 // Service workder js
 minify('./src/service-worker.js', options)
     .then((content) => {
+      // TODO: dynamically up version on build
+      // str.replace(
+      //     /(^const version\=)(\d+)(.*)$/gi,
+      //     (str, pre, ver, post) => pre + (parseInt(ver, 10) + 1) + post);
       fs.writeFile('./public/service-worker.js', content, 'utf8', () => {
         console.log('minify SW done!');
       });
