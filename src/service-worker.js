967d268ddaa222e3bab0803ce9ce204c6ac5bf52
Luis DeLeon <luismdeleon@gmail.com>
2020-12-18 16:54:09 -0800

    20, 21, 22: hotfix several ui issues

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,16 +1,29 @@
-const version = 28;
+const version = 29;
 const staticCacheName = 'site-static-v' + version;
 const dynamicCacheName = 'site-dynamic-v' + version;
-const CACHE_MAX_SIZE = 20;
+const CACHE_MAX_SIZE = 30;
 const assets = [
   '/',
   '/index.html',
   '/robots.txt',
   '/sitemap-file.xml',
+  '/manifest.webmanifest',
+  '/img/icons/icon_32x32.png',
+  '/img/icons/icon_180x180.png',
+  'https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css',
+  'https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@300;900&display=swap',
   '/css/style.css',
   '/img/icons/icon_144x144.png',
   '/img/photos/self.webp',
   '/img/code.webp',
+  '/img/photos/about-me.webp',
+  '/img/concert.webp',
+  '/img/business-coffee-shop.webp',
+  '/img/telecom.webp',
+  '/img/capital.webp',
+  '/img/code-two-screens.webp',
+  '/img/circuit-board.webp',
+  '/img/photos/contact.webp',
   '/__/firebase/8.2.1/firebase-app.js',
   '/__/firebase/8.2.1/firebase-analytics.js',
   '/__/firebase/8.2.1/firebase-firestore.js',
