4badfb55cc2036daa12135f4ca3b1f40fbf87608
Luis <reallyluis@gmail.com>
2021-01-29 07:14:01 -0800

    #80 more static content updates

--- scripts/minify-html.js
+++ scripts/minify-html.js
@@ -12,7 +12,10 @@ const options = {
 const updateContent = (content) => {
   return content
       .replace(/\{MY\_NAME\}/g, staticContent.name)
-      .replace(/\{MY\_TITLE\}/g, staticContent.title);
+      .replace(/\{MY\_TITLE\}/g, staticContent.title)
+      .replace(/\{MY\_EMAIL\}/g, staticContent.email)
+      .replace(/\{MY\_USERNAME\}/g, staticContent.username)
+      .replace(/\{APP\_DOMAIN\}/g, staticContent.domain);
 };
 
 minify('./src/index.html', options)
