b03d2708323319346257ef1ee236a660c18c0831
Luis <reallyluis@gmail.com>
2021-01-29 06:43:14 -0800

    #80 start moving static content to separate file

--- scripts/minify-html.js
+++ scripts/minify-html.js
@@ -1,5 +1,6 @@
 const minify = require('minify');
 const fs = require('fs');
+const staticContent = require('../static-content.json');
 
 const options = {
   html: {
@@ -8,11 +9,21 @@ const options = {
   },
 };
 
+const updateContent = (content) => {
+  return content
+      .replace(/\{MY\_NAME\}/g, staticContent.name)
+      .replace(/\{MY\_TITLE\}/g, staticContent.title);
+};
+
 minify('./src/index.html', options)
     .then((content) => {
-      fs.writeFile('./public/index.html', content, 'utf8', () => {
-        console.log('minify index.html done!');
-      });
+      fs.writeFile(
+          './public/index.html',
+          updateContent(content),
+          'utf8',
+          () => {
+            console.log('minify index.html done!');
+          });
     })
     .catch((err) => {
       console.log('minify index.html failed!');
