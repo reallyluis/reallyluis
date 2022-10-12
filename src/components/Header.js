7b1f858df6c7b77495d4fe9d97030dbb2f527526
Luis <349489+reallyluis@users.noreply.github.com>
2022-10-11 23:57:41 -0700

    35: move script blocks without globals or vars

---
+++ src/components/Header.js
@@ -0,0 +1,45 @@
+(() => {
+  const mediaStatus =
+    window.matchMedia && window.matchMedia("(prefers-color-scheme: dark)");
+  const darkmodeToggle = document.querySelector(".darkmode-toggle__checkbox");
+
+  const setDarkmodeToggle = (isOn = false) => {
+    if (isOn) {
+      document.body.classList.add("darkmode-on");
+    } else {
+      document.body.classList.remove("darkmode-on");
+    }
+
+    if (darkmodeToggle) {
+      darkmodeToggle.checked = !!isOn;
+    }
+  };
+
+  if (darkmodeToggle) {
+    darkmodeToggle.addEventListener("change", () =>
+      setDarkmodeToggle(darkmodeToggle && darkmodeToggle.checked)
+    );
+  }
+
+  if (mediaStatus) {
+    setDarkmodeToggle(mediaStatus.matches);
+
+    mediaStatus.addEventListener("change", (e) => setDarkmodeToggle(e.matches));
+  }
+
+  const navToggle = document.querySelector(".nav-toggle");
+  if (navToggle) {
+    navToggle.addEventListener("click", () => {
+      document.body.classList.toggle("nav-open");
+    });
+  }
+
+  const navLinks = document.querySelectorAll(".nav__link");
+  if (navLinks) {
+    navLinks.forEach((link) => {
+      link.addEventListener("click", () => {
+        document.body.classList.remove("nav-open");
+      });
+    });
+  }
+})();
