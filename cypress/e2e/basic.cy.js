4ba07c0ec5b904c8a706bfdf2d55bd6e5fadde2e
Luis <349489+reallyluis@users.noreply.github.com>
2022-10-17 22:23:30 -0700

    #51: add cypress tests

---
+++ cypress/e2e/basic.cy.js
@@ -0,0 +1,12 @@
+/// <reference types="cypress" />
+
+describe('reallyluis app', () => {
+  beforeEach(() => {
+    cy.visit('https://reallyluis.com');
+  });
+
+  it('displays intro title and subtitle', () => {
+    cy.get('.intro .section-title').should('have.text', '\nHello, I am Luis\n');
+    cy.get('.intro .section-subtitle').should('have.text', 'full-stack developer');
+  });
+});
