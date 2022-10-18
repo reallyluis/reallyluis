a60894c98b72da64991c47d2a23beab81d84af35
Luis <349489+reallyluis@users.noreply.github.com>
2022-10-18 12:56:51 -0700

    update test

--- cypress/e2e/basic.cy.js
+++ cypress/e2e/basic.cy.js
@@ -6,7 +6,7 @@ describe("reallyluis app", () => {
   });
 
   it("displays intro title and subtitle", () => {
-    cy.get(".intro .section-title").should("have.text", "\nHello, I am Luis\n");
+    cy.get(".intro .section-title").should("contains", "Hello, I am Luis");
     cy.get(".intro .section-subtitle").should(
       "have.text",
       "full-stack developer"
