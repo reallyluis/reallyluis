8e658167564b4cd2b5495259fc6e9f1ba313fc45
Luis <349489+reallyluis@users.noreply.github.com>
2022-10-26 22:10:04 -0700

    bug fix and update bot email

--- scripts/bump-package.py
+++ scripts/bump-package.py
@@ -26,8 +26,8 @@ def main():
     json.dump(data, jsonFile, indent=2, sort_keys=True)
     jsonFile.truncate()
 
-  # sys.exit(os.EX_OK)
-  return data["version"]
+  print(data["version"])
+  sys.exit(os.EX_OK)
 
 if __name__ == "__main__":
   main()
