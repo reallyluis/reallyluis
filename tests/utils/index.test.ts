96258b105db70552d95e2e67cf4599ad40126fe4
Luis <349489+reallyluis@users.noreply.github.com>
2022-10-28 19:03:11 -0700

    #58: add jest tests for utils

--- tests/utils/index.test.ts
+++ tests/utils/index.test.ts
@@ -1,10 +1,33 @@
-import expect from "expect";
-import { buildThresholdList } from "@utils/index";
+import { describe, expect, jest, test } from "@jest/globals";
+import { buildThresholdList, capitalizeWord, debounce } from "@utils/index";
 
-describe("buildThresholdList", () => {
-  test("returns array of threshold values", () => {
-    expect(buildThresholdList()).toEqual([
-      0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1,
-    ]);
+describe("utils methods", () => {
+  describe("buildThresholdList", () => {
+    test("returns array of threshold values", () => {
+      expect(buildThresholdList()).toEqual([
+        0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1,
+      ]);
+    });
+  });
+
+  describe("capitalizeWord", () => {
+    test("returns word capitalized", () => {
+      expect(capitalizeWord("hello")).toEqual("Hello");
+    });
+  });
+
+  describe("debounce", () => {
+    test("call debounced function once", () => {
+      jest.useFakeTimers();
+      const func = jest.fn();
+      const deFunc = debounce(func, 1000);
+
+      deFunc();
+      deFunc();
+      deFunc();
+
+      jest.runAllTimers();
+      expect(func).toBeCalledTimes(1);
+    });
   });
 });
