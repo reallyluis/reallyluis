d0fc4295d92565a142b10e3ea941b8dbfe061920
Luis <349489+reallyluis@users.noreply.github.com>
2022-10-30 11:35:32 -0700

    #58: add more cypress tests

---
+++ cypress/e2e/content.cy.js
@@ -0,0 +1,41 @@
+/// <reference types="cypress" />
+
+describe("sections' content", () => {
+  before(() => {
+    cy.visit("/");
+  });
+
+  it("display services content", () => {
+    cy.get(".my-services .services .service:nth-child(1)").contains(
+      "applications these days"
+    );
+    cy.get(".my-services .services .service:nth-child(2)").contains(
+      "More recently I have worked"
+    );
+    cy.get(".my-services .services .service:nth-child(3)").contains(
+      "based data storage"
+    );
+  });
+
+  it("display about content", () => {
+    cy.get(".about-me > div > p:nth-child(1)").contains(
+      "I have worked in several"
+    );
+    cy.get(".about-me > div > p:nth-child(2)").contains("looking forward to");
+  });
+
+  it("display work content", () => {
+    cy.get(".my-work .portfolio > .item:nth-child(1) .title").should(
+      "have.text",
+      "Fundraising"
+    );
+    cy.get(".my-work .portfolio > .item:nth-child(4) .title").should(
+      "have.text",
+      "Telecommunications"
+    );
+  });
+
+  it("display contact content", () => {
+    cy.get("#contact label:nth-child(2) span").should("have.text", "Email");
+  });
+});
---
+++ cypress/e2e/titles.cy.js
@@ -0,0 +1,40 @@
+/// <reference types="cypress" />
+
+describe("sections' title and subtitle", () => {
+  before(() => {
+    cy.visit("/");
+  });
+
+  it("display intro title and subtitle", () => {
+    cy.get(".intro .section-title").contains("Hello");
+    cy.get(".intro .section-subtitle").should(
+      "have.text",
+      "full stack developer"
+    );
+  });
+
+  it("display services title", () => {
+    cy.get(".my-services .section-title").contains("What I do");
+  });
+
+  it("display about title and subtitle", () => {
+    cy.get(".about-me .section-title").contains("Who I am");
+    cy.get(".about-me .section-subtitle").should(
+      "have.text",
+      "Working from a remote location"
+    );
+  });
+
+  it("display work title and subtitle", () => {
+    cy.get(".my-work .section-title").contains("My Work");
+    cy.get(".my-work .section-subtitle").should(
+      "have.text",
+      "A selection of my range of work"
+    );
+  });
+
+  it("display contact title and subtitle", () => {
+    cy.get(".contact-me .section-title").contains("Contact Me");
+    cy.get(".contact-me .section-subtitle").should("have.text", "get in touch");
+  });
+});
