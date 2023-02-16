64a1e041ef0b051a6a502ca5d6f15a6f266010b1
Luis <reallyluis@gmail.com>
2023-02-15 16:59:52 -0800

    Package updates and UI Improvements (#124)
    * #120: update packages
    
    * fix cypress test
    
    * #112: remove hyphen in intro
    
    * #107: fix button animation on hover
    
    * #106: fix smooth scrolling
    
    * #122: self host fonts
    
    * #122: add web font files
    
    * #108: simplify style resets
    
    * #121: improve semantic nav links
    
    * fix lint

--- cypress/e2e/nav.cy.js
+++ cypress/e2e/nav.cy.js
@@ -6,23 +6,23 @@ describe("nav menu", () => {
   });
 
   it("display nav links", () => {
-    cy.get("header .nav .list .item:nth-child(1) .link").should(
+    cy.get("header .nav .list .item:nth-child(1) a").should(
       "contain.text",
       "Home"
     );
-    cy.get("header .nav .list .item:nth-child(2) .link").should(
+    cy.get("header .nav .list .item:nth-child(2) a").should(
       "contain.text",
       "Skills"
     );
-    cy.get("header .nav .list .item:nth-child(3) .link").should(
+    cy.get("header .nav .list .item:nth-child(3) a").should(
       "contain.text",
       "About"
     );
-    cy.get("header .nav .list .item:nth-child(4) .link").should(
+    cy.get("header .nav .list .item:nth-child(4) a").should(
       "contain.text",
       "Work"
     );
-    cy.get("header .nav .list .item:nth-child(5) .link").should(
+    cy.get("header .nav .list .item:nth-child(5) a").should(
       "contain.text",
       "Contact"
     );
