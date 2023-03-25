401d97bfeb372d4855af577ab7f987f60f575f28
Luis <349489+reallyluis@users.noreply.github.com>
2023-03-25 12:29:44 -0700

    #135: init functions

---
+++ functions/hello.ts
@@ -0,0 +1,9 @@
+interface Env {
+	abouts: KVNamespace;
+}
+
+export const onRequest: PagesFunction<Env> = async (context) => {
+	const value = await context.env.abouts.get("1");
+
+  return new Response(value);
+}
