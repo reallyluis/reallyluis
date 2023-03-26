ab7bf52d84f7889c025d92c7f1fdc28239c0ef47
Luis <349489+reallyluis@users.noreply.github.com>
2023-03-25 17:09:54 -0700

    return all content

--- functions/content.ts
+++ functions/content.ts
@@ -4,17 +4,36 @@ interface Env {
   works: KVNamespace;
 }
 
-export const onRequest: PagesFunction<Env> = async (context) => {
+export const getContent = async (context) => {
   const data: { [key: string]: { [key: string]: string } } = {
     abouts: {},
     skills: {},
     works: {},
   };
-  const value = await context.env.abouts.list();
 
-  console.log(data);
+  try {
+    for (const key in data) {
+      const dataSet = context.env[key];
+      const dataList = await dataSet.list();
 
-  return new Response(JSON.stringify(value, null, 2), {
+      for (let i=0; i<dataList.keys.length; i++) {
+        const k = dataList.keys[i];
+        const value = await dataSet.get(k);
+
+        data[key][k] = value;
+      }
+    }
+  } catch (error) {
+    console.error((error as Error)?.message);
+  }
+
+  return data;
+};
+
+export const onRequest: PagesFunction<Env> = async (context) => {
+  const data: { [key: string]: { [key: string]: string } } = await getContent(context);
+
+  return new Response(JSON.stringify(data, null, 2), {
     headers: {
       "Content-Type": "application/json;charset=UTF-8",
       "Cache-Control": "public, max-age=86400",
